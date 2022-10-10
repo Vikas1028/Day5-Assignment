819e940cf502b90cbd68abd0f575a04105aebe47
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-10-10 16:30:15 +0530

    Swap Two Numbers

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,17 @@
+
+public class SwapTwoNumbers {
+
+	public static void main(String args[]) {
+		int a,b,c;
+		a=10;
+		b=20;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		c=b;
+		b=a;
+		a=c;
+		System.out.println("swap a and b");
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+	}
+}
